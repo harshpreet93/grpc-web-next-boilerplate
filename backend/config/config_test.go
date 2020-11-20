5e42cd9892a8e87c4f8db081509fd5a315620c70
Harshpreet Singh <singhharshpreet1993@gmail.com>
2020-11-19 23:53:28 -0800

    more test

--- backend/config/config_test.go
+++ backend/config/config_test.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"reflect"
 	"testing"
+
+	"github.com/harshpreet93/grpc-web-next-boilerplate/backend/util"
 )
 
 func TestConfig_New(t *testing.T) {
@@ -18,7 +20,7 @@ func TestConfig_New(t *testing.T) {
 		args args
 		want struct {
 			conf Config
-			err  error
+			err  string
 		}
 	}{
 		{
@@ -26,31 +28,31 @@ func TestConfig_New(t *testing.T) {
 			args: args{env: "test"},
 			want: struct {
 				conf Config
-				err  error
-			}{Config{"this_is_a_password"}, nil},
+				err  string
+			}{Config{"this_is_a_password"}, ""},
 		},
 		{
 			name: "Config is not found and fallback is triggered",
 			args: args{env: "doesnotexist"},
 			want: struct {
 				conf Config
-				err  error
-			}{Config{"devpass"}, nil},
+				err  string
+			}{Config{"devpass"}, ""},
 		},
-		// {
-		// 	name: "Config found, but is invalid",
-		// 	args: args{env: "testInvalid"},
-		// 	want: struct {
-		// 		conf Config
-		// 		err  error
-		// 	}{Config{}, nil},
-		// },
+		{
+			name: "Config found, but is invalid",
+			args: args{env: "testInvalid"},
+			want: struct {
+				conf Config
+				err  string
+			}{Config{}, "invalid character"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c := Config{}
 			log.Printf("env is %s", tt.args.env)
-			if gotConfig, gotErr := c.New(tt.args.env); !reflect.DeepEqual(gotConfig, tt.want.conf) || !reflect.DeepEqual(gotErr, tt.want.err) {
+			if gotConfig, gotErr := c.New(tt.args.env); !reflect.DeepEqual(gotConfig, tt.want.conf) || !util.ErrorContains(gotErr, tt.want.err) {
 				t.Errorf("Config.New() = %v, err = %v, want %v", gotConfig, gotErr, tt.want)
 			}
 		})
---
+++ backend/util/util.go
@@ -0,0 +1,18 @@
+package util
+
+import "strings"
+
+// ErrorContains checks if the error message in out contains the text in
+// want.
+//
+// This is safe when out is nil. Use an empty string for want if you want to
+// test that err is nil.
+func ErrorContains(out error, want string) bool {
+	if out == nil {
+		return want == ""
+	}
+	if want == "" {
+		return false
+	}
+	return strings.Contains(out.Error(), want)
+}
